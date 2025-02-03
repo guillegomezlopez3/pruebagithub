132c5ed990e52c8e6a5bd190579d9c40853f5bcd
guille <guillegomezlopez3@gmail.com>
2025-02-03 18:36:40 +0100

    pruebagit

---
+++ pruebaparagit/src/pruebaparagit/Pruebaparagit.java
@@ -0,0 +1,10 @@
+package pruebaparagit;
+
+public class Pruebaparagit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es una prueba para git");
+	}
+
+}
